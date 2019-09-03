bd940e5614393ef9ba133045d4c2321200516c90
akitsu-sanae <akitsu.sanae@gmail.com>
2019-09-03 14:35:15 +0900

    update frame

--- src/frame.rs
+++ src/frame.rs
@@ -1,17 +1,64 @@
+use crate::util;
 use arrayvec::ArrayVec;
 use std::collections::VecDeque;
 use std::error::Error;
+use std::fmt;
 
 #[derive(Debug, Clone)]
 pub struct Bytes(VecDeque<u8>);
 
 const MAC_ADDR_LEN: usize = 6;
-const IPv4_ADDR_LEN: usize = 4;
-const IPv6_ADDR_LEN: usize = 16;
+const IPV4_ADDR_LEN: usize = 4;
+const IPV6_ADDR_LEN: usize = 16;
 
+#[derive(Debug, Clone)]
 pub struct MacAddr([u8; MAC_ADDR_LEN]);
-pub struct Ipv4Addr([u8; IPv4_ADDR_LEN]);
-pub struct Ipv6Addr([u8; IPv6_ADDR_LEN]);
+
+impl MacAddr {
+    pub fn from_str(str: &String) -> Result<Self, Box<dyn Error>> {
+        str.split(':')
+            .map(|n| u8::from_str_radix(n, 16))
+            .collect::<Result<ArrayVec<[_; 6]>, _>>()
+            .map(|arr| Self(arr.into_inner().unwrap()))
+            .map_err(|err| Box::new(util::RuntimeError::new(format!("{}", err))) as Box<dyn Error>)
+    }
+}
+
+impl fmt::Display for MacAddr {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(
+            f,
+            "{:#X}:{:#X}:{:#X}:{:#X}:{:#X}:{:#X}",
+            self.0[0], self.0[1], self.0[2], self.0[3], self.0[4], self.0[5]
+        )
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct Ipv4Addr([u8; IPV4_ADDR_LEN]);
+
+impl Ipv4Addr {
+    pub fn from_str(str: String) -> Result<Self, Box<dyn Error>> {
+        str.split(':')
+            .map(|n| u8::from_str_radix(n, 10))
+            .collect::<Result<ArrayVec<[_; 4]>, _>>()
+            .map(|arr| Self(arr.into_inner().unwrap()))
+            .map_err(|err| Box::new(util::RuntimeError::new(format!("{}", err))) as Box<dyn Error>)
+    }
+}
+
+impl fmt::Display for Ipv4Addr {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(
+            f,
+            "{:#X}.{:#X}.{:#X}.{:#X}",
+            self.0[0], self.0[1], self.0[2], self.0[3]
+        )
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct Ipv6Addr([u8; IPV6_ADDR_LEN]);
 
 impl Bytes {
     pub fn new(max_len: usize) -> Self {
@@ -48,18 +95,18 @@ impl Bytes {
         }
     }
     pub fn pop_ipv4_addr(&mut self) -> Option<Ipv4Addr> {
-        if IPv4_ADDR_LEN <= self.0.len() {
-            let arr_vec: ArrayVec<[_; IPv4_ADDR_LEN]> =
-                self.0.split_off(IPv4_ADDR_LEN).into_iter().collect();
+        if IPV4_ADDR_LEN <= self.0.len() {
+            let arr_vec: ArrayVec<[_; IPV4_ADDR_LEN]> =
+                self.0.split_off(IPV4_ADDR_LEN).into_iter().collect();
             Some(Ipv4Addr(arr_vec.into_inner().unwrap()))
         } else {
             None
         }
     }
     pub fn pop_ipv6_addr(&mut self) -> Option<Ipv6Addr> {
-        if IPv6_ADDR_LEN <= self.0.len() {
-            let arr_vec: ArrayVec<[_; IPv6_ADDR_LEN]> =
-                self.0.split_off(IPv6_ADDR_LEN).into_iter().collect();
+        if IPV6_ADDR_LEN <= self.0.len() {
+            let arr_vec: ArrayVec<[_; IPV6_ADDR_LEN]> =
+                self.0.split_off(IPV6_ADDR_LEN).into_iter().collect();
             Some(Ipv6Addr(arr_vec.into_inner().unwrap()))
         } else {
             None
