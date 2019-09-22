371dc19bd220bc0144434ad2cefe92f0b6d93a24
akitsu-sanae <akitsu.sanae@gmail.com>
2019-09-22 22:35:19 +0900

    fix ethernet test

--- examples/ethernet_test.rs
+++ examples/ethernet_test.rs
@@ -1,27 +1,33 @@
+#[macro_use]
+extern crate lazy_static;
+extern crate libc;
 extern crate microps_rs;
 extern crate nix;
 
-use microps_rs::{device::Device, ethernet, raw};
-use nix::sys::signal;
-use std::sync::{Arc, Mutex};
+use nix::sys::signal::{self, SigHandler, Signal};
+use std::sync::atomic::{AtomicBool, Ordering};
+use microps_rs::{ethernet, raw};
+
+lazy_static! {
+    static ref TERMINATE: AtomicBool = AtomicBool::new(false);
+}
+
+extern "C" fn handle_sigint(signal: libc::c_int) {
+    let signal = Signal::from_c_int(signal).unwrap();
+    TERMINATE.store(signal == Signal::SIGINT, Ordering::Relaxed);
+}
 
 fn main() {
     let args: Vec<String> = ::std::env::args().into_iter().collect();
     if args.len() != 2 {
         panic!("USAGE: ethernet_test <device>");
     }
-    let mut sigset = signal::SigSet::empty();
-    sigset.add(signal::Signal::SIGINT);
-    signal::sigprocmask(signal::SigmaskHow::SIG_BLOCK, Some(&sigset), None).unwrap();
-    let mut device = Arc::new(Mutex::new(
-        ethernet::EthernetDevice::open(args[1].as_str(), raw::Type::Auto).unwrap(),
-    ));
-    device.run().unwrap();
-    loop {
-        if signal::Signal::SIGINT == sigset.wait().unwrap() {
-            break;
-        }
-    }
 
+    let handler = SigHandler::Handler(handle_sigint);
+    unsafe { signal::signal(Signal::SIGINT, handler) }.unwrap();
+
+    let device = ethernet::Device::open(args[1].as_str(), raw::Type::Auto).unwrap();
+    device.run().unwrap();
+    while !TERMINATE.load(Ordering::SeqCst) {}
     device.close().unwrap();
 }
