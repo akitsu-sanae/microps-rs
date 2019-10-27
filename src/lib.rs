f60f370ba628c28bf92e75dd08cb8b6859da2e36
akitsu-sanae <akitsu.sanae@gmail.com>
2019-10-27 16:13:56 +0900

    remove waste codes

--- src/lib.rs
+++ src/lib.rs
@@ -14,36 +14,14 @@ extern crate lazy_static;
 #[macro_use]
 extern crate nix;
 
-use std::error::Error;
-
 pub mod arp;
 pub mod ethernet;
 pub mod frame;
 pub mod icmp;
-pub mod ip;
-pub mod device;
 pub mod interface;
-pub mod net;
+pub mod ipv4;
 pub mod raw;
 pub mod slip;
 pub mod tcp;
 pub mod udp;
 pub mod util;
-
-use std::sync::{Arc, Mutex};
-
-pub struct Microps {
-    pub devices: Vec<Arc<Mutex<dyn device::Device>>>,
-}
-
-impl Microps {
-    pub fn new() -> Result<Self, Box<dyn Error>> {
-        Ok(Microps {
-            devices: vec![],
-        })
-    }
-}
-
-impl Drop for Microps {
-    fn drop(&mut self) {}
-}
